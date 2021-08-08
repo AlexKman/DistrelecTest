5f010ef8205efdd096c3b6d3d74e8e611b61c25c
Alex Keeley <keeley.lp@gmail.com>
2021-08-08 20:09:51 +0100

    Updated navbar.js to include a basic reactbootstrap dark navbar including an image brand and home link

--- src/src/components/Navbar.js
+++ src/src/components/Navbar.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export default function Navbar() {
-  return <div></div>;
+export default function Navigationbar() {
+  return (
+    <Navbar expand="lg" sticky="top" bg="dark" variant="dark">
+      <Navbar.Brand>
+        <img alt="logo" class="navbarImg" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link>
+            <Link className="nav-link" to="/">
+              <i class="fas fa-home"></i>
+              Home
+            </Link>
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
