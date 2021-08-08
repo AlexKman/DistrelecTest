874c3242d5a1b24d318d159556d0c18451bef702
Alex Keeley <keeley.lp@gmail.com>
2021-08-08 22:02:13 +0100

    Duplicated productcard

--- src/src/components/ProductCard.js
+++ src/src/components/ProductCard.js
@@ -3,26 +3,52 @@ import "./ProductCard.scss";
 
 export default function ProductCard() {
   return (
-    <div class="card">
-      <div class="row no-gutters card-inner-container">
-        <div class="col-auto ">
-          <img
-            src={process.env.PUBLIC_URL + "/images/DistrelecPart.jpg"}
-            class="product-image"
-            alt=""
-          ></img>
+    <div>
+      <div class="card">
+        <div class="row no-gutters card-inner-container">
+          <div class="col-auto ">
+            <img
+              src={process.env.PUBLIC_URL + "/images/DistrelecPart.jpg"}
+              class="product-image"
+              alt=""
+            ></img>
+          </div>
+          <div class="col">
+            <div class="card-block px-2">
+              <h4 class="card-title">ABX00023 - Arduino MKR WIFI 1010</h4>
+              <p class="card-text">
+                The MKR WIFI 1010 is a significant improvement on the MKR 1000
+                WIFI. It's equipped with an ESP32 module made by U-BLOX.
+                <br></br>
+                <br></br> This board aims to speed up and simplify the
+                prototyping of WiFi based IoT applications thanks to the
+                flexibility of the ESP32 module and its low power consumption.
+              </p>
+            </div>
+          </div>
         </div>
-        <div class="col">
-          <div class="card-block px-2">
-            <h4 class="card-title">ABX00023 - Arduino MKR WIFI 1010</h4>
-            <p class="card-text">
-              The MKR WIFI 1010 is a significant improvement on the MKR 1000
-              WIFI. It's equipped with an ESP32 module made by U-BLOX.
-              <br></br>
-              <br></br> This board aims to speed up and simplify the prototyping
-              of WiFi based IoT applications thanks to the flexibility of the
-              ESP32 module and its low power consumption.
-            </p>
+      </div>
+      <div class="card">
+        <div class="row no-gutters card-inner-container">
+          <div class="col-auto ">
+            <img
+              src={process.env.PUBLIC_URL + "/images/DistrelecPart.jpg"}
+              class="product-image"
+              alt=""
+            ></img>
+          </div>
+          <div class="col">
+            <div class="card-block px-2">
+              <h4 class="card-title">ABX00023 - Arduino MKR WIFI 1010</h4>
+              <p class="card-text">
+                The MKR WIFI 1010 is a significant improvement on the MKR 1000
+                WIFI. It's equipped with an ESP32 module made by U-BLOX.
+                <br></br>
+                <br></br> This board aims to speed up and simplify the
+                prototyping of WiFi based IoT applications thanks to the
+                flexibility of the ESP32 module and its low power consumption.
+              </p>
+            </div>
           </div>
         </div>
       </div>
