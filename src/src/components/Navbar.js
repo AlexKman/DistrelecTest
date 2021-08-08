f84f9ea57ab1c78c6987f120be055903975764ee
Alex Keeley <keeley.lp@gmail.com>
2021-08-08 20:57:52 +0100

    Added navbar.scss file with styling to reduce navbar image size

--- src/src/components/Navbar.js
+++ src/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "./Navbar.scss";
 
 export default function Navigationbar() {
   return (
