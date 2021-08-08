893a0096acb34b837de26d6c85d8e55d77deb7eb
Alex Keeley <keeley.lp@gmail.com>
2021-08-08 22:10:49 +0100

    Added Carousel.js and Carousel.scss

--- src/src/components/ProductCard.js
+++ src/src/components/ProductCard.js
@@ -5,10 +5,10 @@ export default function ProductCard() {
   return (
     <div>
       <div class="card">
-        <div class="row no-gutters card-inner-container">
-          <div class="col-auto ">
+        <div class="row no-gutters">
+          <div class="col-auto">
             <img
-              src={process.env.PUBLIC_URL + "/images/DistrelecPart.jpg"}
+              src={process.env.PUBLIC_URL + "/images/DistrelecPart1.jpg"}
               class="product-image"
               alt=""
             ></img>
@@ -18,35 +18,8 @@ export default function ProductCard() {
               <h4 class="card-title">ABX00023 - Arduino MKR WIFI 1010</h4>
               <p class="card-text">
                 The MKR WIFI 1010 is a significant improvement on the MKR 1000
-                WIFI. It's equipped with an ESP32 module made by U-BLOX.
-                <br></br>
-                <br></br> This board aims to speed up and simplify the
-                prototyping of WiFi based IoT applications thanks to the
-                flexibility of the ESP32 module and its low power consumption.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="card">
-        <div class="row no-gutters card-inner-container">
-          <div class="col-auto ">
-            <img
-              src={process.env.PUBLIC_URL + "/images/DistrelecPart.jpg"}
-              class="product-image"
-              alt=""
-            ></img>
-          </div>
-          <div class="col">
-            <div class="card-block px-2">
-              <h4 class="card-title">ABX00023 - Arduino MKR WIFI 1010</h4>
-              <p class="card-text">
-                The MKR WIFI 1010 is a significant improvement on the MKR 1000
-                WIFI. It's equipped with an ESP32 module made by U-BLOX.
-                <br></br>
-                <br></br> This board aims to speed up and simplify the
-                prototyping of WiFi based IoT applications thanks to the
-                flexibility of the ESP32 module and its low power consumption.
+                WIFI. This board aims to speed up and simplify the prototyping
+                of WiFi based IoT applications.
               </p>
             </div>
           </div>
