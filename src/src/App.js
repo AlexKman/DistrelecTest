a847797642a3d18422c2ad807adc22bbb9f21cc0
Alex Keeley <keeley.lp@gmail.com>
2021-08-08 20:07:52 +0100

    Added basic routing to App component using reactrouter, Home component to display on / endpoint

--- src/src/App.js
+++ src/src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import Navigationbar from "./components/Navbar.js";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Navigationbar></Navigationbar>
+      <Switch>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
