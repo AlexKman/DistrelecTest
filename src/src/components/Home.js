28e7ad643bbea95ccf8aa93a0ffebfb914e15462
Alex Keeley <keeley.lp@gmail.com>
2021-08-09 22:11:59 +0100

    Updated styling around Home component

--- src/src/components/Home.js
+++ src/src/components/Home.js
@@ -3,8 +3,21 @@ import "./Home.scss";
 
 export default function Home() {
   return (
-    <div className="container">
-      <div className="carousel-item active"></div>
+    <div className="home-container">
+      <div className="carousel-item active">
+        <div className="container">
+          <h1>Lorem Ipsum. Blah Blah blah.</h1>
+          <p>
+            Lorem Ipsum. Blah Blah blah.Lorem Ipsum. Blah Blah blah. Lorem
+            Ipsum. Blah Blah blah.Lorem Ipsum. Blah Blah blah. Lorem Ipsum. Blah
+            Blah blah.Lorem Ipsum. Blah Blah blah. Lorem Ipsum. Blah Blah
+            blah.Lorem Ipsum. Blah Blah blah.{" "}
+          </p>
+          <a href="#" className="bt btn-lg btn-primary">
+            Click me
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
