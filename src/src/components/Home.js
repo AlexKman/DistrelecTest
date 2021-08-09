67db1d3445225aaca15d904eb6e0fafd1aee6324
Alex Keeley <keeley.lp@gmail.com>
2021-08-09 22:13:58 +0100

    Added extra divs with classNames to contain the lower carousel parts

--- src/src/components/Home.js
+++ src/src/components/Home.js
@@ -4,18 +4,22 @@ import "./Home.scss";
 export default function Home() {
   return (
     <div className="home-container">
-      <div className="carousel-item active">
-        <div className="container">
-          <h1>Lorem Ipsum. Blah Blah blah.</h1>
-          <p>
-            Lorem Ipsum. Blah Blah blah.Lorem Ipsum. Blah Blah blah. Lorem
-            Ipsum. Blah Blah blah.Lorem Ipsum. Blah Blah blah. Lorem Ipsum. Blah
-            Blah blah.Lorem Ipsum. Blah Blah blah. Lorem Ipsum. Blah Blah
-            blah.Lorem Ipsum. Blah Blah blah.{" "}
-          </p>
-          <a href="#" className="bt btn-lg btn-primary">
-            Click me
-          </a>
+      <div className="carousel slide">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <div className="container">
+              <h1>Lorem Ipsum. Blah Blah blah.</h1>
+              <p>
+                Lorem Ipsum. Blah Blah blah.Lorem Ipsum. Blah Blah blah. Lorem
+                Ipsum. Blah Blah blah.Lorem Ipsum. Blah Blah blah. Lorem Ipsum.
+                Blah Blah blah.Lorem Ipsum. Blah Blah blah. Lorem Ipsum. Blah
+                Blah blah.Lorem Ipsum. Blah Blah blah.{" "}
+              </p>
+              <a href="#" className="bt btn-lg btn-primary">
+                Click me
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
