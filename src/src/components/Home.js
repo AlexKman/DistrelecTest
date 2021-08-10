fafe5fb2d2e40d201e7269c4c9b4c9b9336268c6
Alex Keeley <keeley.lp@gmail.com>
2021-08-10 16:12:15 +0100

    Added Media query to account for mobile laptop sizes on image size

--- src/src/components/Home.js
+++ src/src/components/Home.js
@@ -3,6 +3,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import data from "../data.json";
+import "./Home.scss";
 
 export default function Home() {
   const settings = {
@@ -14,12 +15,17 @@ export default function Home() {
   };
   return (
     <div className="container">
+      <h1>Related Products</h1>
       <Slider {...settings}>
         {data.carouselData.map((obj) => {
           return (
             <div>
               {" "}
-              <img src={obj.productImageUrl} alt={obj.code} />
+              <img
+                className="carousel-image"
+                src={obj.productImageUrl}
+                alt={obj.code}
+              />
             </div>
           );
         })}
